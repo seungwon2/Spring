0e05bc59db6d0b713bcb4347a15596ff01713088
seungwon2 <dandelionsw2@ewhain.net>
2021-04-20 18:33:47 +0900

    :sparkles: feat : add orderservie implementation

---
+++ src/main/java/firstProject/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package firstProject.core.order;
+
+import firstProject.core.discount.DiscountPolicy;
+import firstProject.core.discount.FixDiscountPolicy;
+import firstProject.core.member.Member;
+import firstProject.core.member.MemberRepository;
+import firstProject.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService {
+
+    //이 서비스에서는 멤버 조회와 할인 정책 적용이 필요
+    //레포지토리와 할인 정책을 가져옴
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        //멤버 아이디로 멤버를 조회해서 꺼내옴
+        Member member = memberRepository.findById(memberId);
+        //discountPolicy에서 알아서 member로 grade를 정의함
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        //할인된 가격을 적용해서 retrun
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
