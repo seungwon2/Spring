ffaf5e72b60fd12e5f0590be9e7ea0dd3f560e6f
seungwon2 <dandelionsw2@ewhain.net>
2021-04-20 19:46:21 +0900

    :sparkles: feat : impl inject by constructor

--- src/main/java/firstProject/core/AppConfig.java
+++ src/main/java/firstProject/core/AppConfig.java
@@ -11,7 +11,7 @@ public class AppConfig {
     //appconfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.(온갖 impl들)
     //appconfig는 생성한 객체 인스턴스의 레퍼런스를 생성자를 통해서 주입해준다.
     public MemberService memberService(){
-        return new MemberServiceImpl() (new MemoryMemberRepository());
+        return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
     public OrderService orderService(){
--- src/main/java/firstProject/core/member/MemberServiceImpl.java
+++ src/main/java/firstProject/core/member/MemberServiceImpl.java
@@ -2,19 +2,22 @@ package firstProject.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
-    //앞에는 인터페이스, new한 후에는 구현체로 객체 생성!
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    //인터페이스를 선언만
+    private final MemberRepository memberRepository;
+
+    //생성자 만들면서 구현체를 생성 - app config에서 나중에 주입하도록 만듦
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     //회원가입
     //memberRepository 안에 있는 save 메소드를 이렇게 이용
-    @Override
     public void join(Member member) {
         memberRepository.save(member);
     }
 
     //회원 검색
     //memberRepository 내부 findById 메소드를 이렇게 이용
-    @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
