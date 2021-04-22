75522d4969bc1315d93982f985c7238975cc1ed5
seungwon2 <dandelionsw2@ewhain.net>
2021-04-22 12:09:00 +0900

    :sparkles: feat : add non-singleton code

---
+++ src/test/java/firstProject/core/singleton/SingletonTest.java
@@ -0,0 +1,27 @@
+package firstProject.core.singleton;
+
+import firstProject.core.AppConfig;
+import firstProject.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+    //싱글톤 패턴을 사용하지 않으면 고객이 요청할 때마다 매번 새로 객체를 생성해 메모리 낭비가 심하다.
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+        //참조값이 다른 것을 확인
+        //참조값이 다르다 == 매번 새로 객체를 생성한다.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+        //memberService1 != memberService2
+        assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
