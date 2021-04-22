a110f2ba430913fa6b212a3b114a14c7afc01a4d
seungwon2 <dandelionsw2@ewhain.net>
2021-04-22 12:40:09 +0900

    :sparkles: feat : check appconfig as a spring bean

--- src/test/java/firstProject/core/singleton/SingletonTest.java
+++ src/test/java/firstProject/core/singleton/SingletonTest.java
@@ -60,5 +60,14 @@ public class SingletonTest {
     //memberService1 == memberService2
         assertThat(memberService1).isSameAs(memberService2);
     }
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new
+                AnnotationConfigApplicationContext(AppConfig.class);
+        //AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+        //출력: bean = class hello.core.AppConfig$$EnhancerBySpringCGLIB$$bd479d70
+    }
 
 }
