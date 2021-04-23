c0ed3aaaf4200b54d15c4b037ddaa4190486cbc9
seungwon2 <dandelionsw2@ewhain.net>
2021-04-23 13:15:36 +0900

    :sparkles: feat : test auto configuration

---
+++ src/test/java/firstProject/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package firstProject.core.scan;
+
+import firstProject.core.AutoAppConfig;
+import firstProject.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
